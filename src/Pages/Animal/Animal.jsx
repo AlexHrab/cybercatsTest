df06b43bf6cb024255dd0fffc126f11edf4b9b06
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2025-02-20 10:00:00 +0200

    fix

--- src/Pages/Animal/Animal.jsx
+++ src/Pages/Animal/Animal.jsx
@@ -1,13 +1,11 @@
 import css from "./Animal.module.css";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useMemo } from "react";
 import clsx from "clsx";
 
 export function Animal({ selected, values }) {
-  const [animal, setAnimal] = useState(null);
   const location = useLocation();
-  const thisLocation = location.pathname === "/" ? true : false;
+  const thisLocation = location.pathname === "/";
 
   const container = clsx(css.container, !thisLocation && css.animalContainer);
   const innerContainer = clsx(
@@ -15,33 +13,25 @@ export function Animal({ selected, values }) {
     !thisLocation && css.animalInnerContainer
   );
 
-  useEffect(() => {
-    const foundAnimal = values.find((el) => el.name === selected);
-    setAnimal(foundAnimal);
-  }, [selected, values]);
+  const animal = useMemo(
+    () => values.find((el) => el.name === selected) || null,
+    [selected, values]
+  );
+
+  if (!animal) return <p className={css.title}>No animal selected</p>;
 
   return (
-    <>
-      {animal ? (
-        <div className={container}>
-          <div className={innerContainer}>
-            <Link
-              to={thisLocation ? `/${selected}` : `/`}
-              className={css.button}
-            >
-              {thisLocation ? "Info" : "Back"}
-            </Link>
+    <div className={container}>
+      <div className={innerContainer}>
+        <Link to={thisLocation ? `/${selected}` : `/`} className={css.button}>
+          {thisLocation ? "Info" : "Back"}
+        </Link>
 
-            <img className={css.image} src={animal.avatar} alt={animal.name} />
-            <h2 className={css.text}>{animal.name}</h2>
-          </div>
-          {!thisLocation && (
-            <p className={css.description}>{animal.description}</p>
-          )}
-        </div>
-      ) : (
-        <p className={css.title}>No animal selected</p>
-      )}
-    </>
+        <img className={css.image} src={animal.avatar} alt={animal.name} />
+        <h2 className={css.text}>{animal.name}</h2>
+      </div>
+
+      {!thisLocation && <p className={css.description}>{animal.description}</p>}
+    </div>
   );
 }
